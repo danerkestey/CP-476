bccbe6d73bc13eb01088d8242f3ad79c2324dd4a
Daner Yasin <danerkyasin@gmail.com>
2023-07-05 17:29:32 -0400

    new set up

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CP-476
-Simple CRUD app for our Internet Computing class that interacts with a Apache web server and MySQL database
+Simple CRUD app for our Internet Computing class that uses the LAMP stack.
+
+MAKE SURE THAT USERS ARE NTO ABLE TO INJECT SQL QUERIES AND IT REJECTS IT
---
+++ php/utils/database.php
@@ -0,0 +1,18 @@
+<?
+
+$host = 'localhost';
+$db = 'CP476_Database';
+$user = 'root';
+$pass = 'password'; // this just for the demo
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES		=> false,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+?>
