e847dd410401ab144b7c7e8c1e86f5086b5038dd
Daner Yasin <77366878+danerkestey@users.noreply.github.com>
2023-07-20 19:19:58 -0400

    refresh refactoring and changes

--- php/actions/refresh.php
+++ php/actions/refresh.php
@@ -31,33 +31,59 @@ switch ($table) {
     case 'Product':
         log_message('REFRESH: inside switch statement for Product');
         $tableName = 'ProductTable';
+        $results = refreshTable($pdo, $tableName);
         break;
     case 'Supplier':
         log_message('REFRESH: inside switch statement for Supplier');
         $tableName = 'SupplierTable';
+        $results = refreshTable($pdo, $tableName);
         break;
-    default: // Default to inventory table
-        log_message('REFRESH: inside switch statement for Inventory/Default');
+    case 'Inventory':
+        log_message('REFRESH: inside switch statement for Inventory');
         $tableName = 'InventoryTable';
+        $results = refreshInventoryTable($pdo);
         break;
+
+    default: // Default to invalid table
+        log_message('REFRESH: inside switch statement for Default');
+        echo json_encode(['status' => 'error', 'message' => 'Invalid table']);
+        exit;
 }
 
-log_message('');
-log_message('');
-log_message('REFRESH: The SQL Query is: ' . "SELECT * FROM $tableName");
-// Prepare a SQL statement to get all items from the specified table
-$stmt = $pdo->prepare("SELECT * FROM $tableName");
-log_message('REFRESH: after prepare statement');
+function refreshTable($pdo, $tableName) {
+    log_message('REFRESH: The SQL Query is: ' . "SELECT * FROM $tableName");
+    
+    // Prepare a SQL statement to get all items from the specified table
+    $stmt = $pdo->prepare("SELECT * FROM $tableName");
+    log_message('REFRESH: after prepare statement');
 
-log_message('REFRESH: before execute statement');
-// Execute the statement
-$stmt->execute();
-log_message('REFRESH: after execute statement');
+    log_message('REFRESH: before execute statement');
+    // Execute the statement
+    $stmt->execute();
+    log_message('REFRESH: after execute statement');
 
-log_message('REFRESH: before fetchAll statement');
-// Fetch all items from the table
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-log_message('REFRESH: after fetchAll statement');
+    log_message('REFRESH: before fetchAll statement');
+    // Fetch all items from the table
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    log_message('REFRESH: after fetchAll statement');
+
+    return $results;
+}
+
+function refreshInventoryTable($pdo) {
+    log_message('REFRESH: Refresh Inventory Table function');
+    
+    // The SQL query to retrieve data for the Inventory table
+    $sql = "SELECT p.ProductID, p.ProductName, p.Quantity, p.Price, p.Status, s.SupplierName
+            FROM ProductTable p
+            INNER JOIN SupplierTable s ON p.SupplierID = s.SupplierID";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    // Fetch all items from the inventory table
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
 if($results) {
     log_message('REFRESH: results found');
@@ -68,4 +94,5 @@ if($results) {
     echo json_encode(['status' => 'error', 'message' => 'No items found']);
     exit;
 }
+
 ?>
