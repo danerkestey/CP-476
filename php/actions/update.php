c8452dc9144ea30c4336cc243426deea2028d378
mubin028 <93798596+mubin028@users.noreply.github.com>
2023-07-26 22:05:15 -0400

    new update function and added buttons

--- php/actions/update.php
+++ php/actions/update.php
@@ -1,22 +1,73 @@
 <?php
-require_once '../utils/database.php';
+require_once(__DIR__ . '/../utils/logger.php');
+log_message('UPDATE: at the start');
+
+ini_set('display_errors', 0);
+error_reporting(0);
+
+// Catch all errors and exceptions
+set_error_handler(function($errno, $errstr, $errfile, $errline) {
+    log_message('error in set_error_handler: ' . $errstr . ' in ' . $errfile . ' on line ' . $errline . 'error');
+    echo json_encode(['status' => 'error', 'message' => $errstr]);
+    exit;
+}, E_ALL);
+
+set_exception_handler(function($exception) {
+    log_message('error in set_exception_handler: ' . $exception->getMessage() . 'error');
+    echo json_encode(['status' => 'error', 'message' => $exception->getMessage()]);
+    exit;
+});
 
 // Capture POST data
-$productId = $_POST['productId'];
-$newQuantity = $_POST['newQuantity'];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["productID"]) && isset($_POST["quantity"])) {
+        $productID = $_POST["productID"];
+        $quantity = $_POST["quantity"];
 
-// Prepare a SQL statement to update the quantity of a product in the ProductTable
-$stmt = $pdo->prepare("UPDATE ProductTable SET Quantity = :newQuantity WHERE ProductID = :productId");
+        require_once __DIR__.'/../utils/database.php';
 
-// Bind the parameters and execute the statement
-$stmt->bindParam(':newQuantity', $newQuantity, PDO::PARAM_INT);
-$stmt->bindParam(':productId', $productId, PDO::PARAM_INT);
-$stmt->execute();
+        try {
+            log_message('UPDATE: before getting connection');
+            // Create a new PDO instance
+            $pdo = getConnection();
 
-// Check if the update was successful
-if($stmt->rowCount()) {
-    echo json_encode(['status' => 'success', 'message' => 'Quantity updated successfully']);
-} else {
-    echo json_encode(['status' => 'error', 'message' => 'Failed to update quantity']);
+            // Check if the quantity is numeric and non-negative
+            if (!is_numeric($quantity) || $quantity < 0) {
+                echo json_encode(['status' => 'error', 'message' => 'Invalid quantity. Quantity must be a non-negative number.']);
+                exit;
+            }
+
+            // Check if the quantity is not blank
+            if ($quantity === '') {
+                echo json_encode(['status' => 'error', 'message' => 'Quantity cannot be blank.']);
+                exit;
+            }
+
+            // If the quantity is updated to 0, delete the entry from the table
+            if ($quantity == 0) {
+                $stmt = $pdo->prepare("DELETE FROM ProductTable WHERE ProductID = :productID");
+                $stmt->bindParam(':productID', $productID, PDO::PARAM_INT);
+                $stmt->execute();
+
+                echo json_encode(['status' => 'success', 'message' => 'Product deleted successfully.']);
+                exit;
+            }
+
+            // Prepare and execute the update query
+            $stmt = $pdo->prepare("UPDATE ProductTable SET Quantity = :quantity WHERE ProductID = :productID");
+            $stmt->bindParam(':quantity', $quantity, PDO::PARAM_INT);
+            $stmt->bindParam(':productID', $productID, PDO::PARAM_INT);
+            $stmt->execute();
+
+            log_message('UPDATE: after execute');
+            echo json_encode(['status' => 'success', 'message' => 'Quantity updated successfully']);
+        } catch (PDOException $error) {
+            log_message('UPDATE: inside catch PDOException: ' . $error->getMessage() . 'error');
+            echo json_encode(['status' => 'error', 'message' => 'Failed to update quantity']);
+        }
+    } else {
+        log_message('UPDATE: Invalid input data');
+        echo json_encode(['status' => 'error', 'message' => 'Invalid input data']);
+    }
 }
 ?>
